a4a6adff9f83dfa2d2b5552b6591bc8928539692
darprai <darioc.2000@gmail.com>
2025-12-06 20:47:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,14 @@
-const cacheName = "pie-3lvl-cache-v2";
+const cacheName = "pie-3lvl-cache-v3"; // Versione aggiornata
 
 const assets = [
   "index.html",
   "style.css",
+  "rects.js", // NUOVO
   "engine.js",
-  "entities.js",
-  "game.js",
+  "projectile.js", // NUOVO
+  "player.js", // NUOVO
   "boss_final.js",
+  "game.js",
   "ending.js",
   "manifest.json",
 
@@ -18,6 +20,13 @@ const assets = [
   "assets/sprites/icon-192.png",
   "assets/sprites/icon-512.png",
   "assets/sprites/favicon.ico",
+  "assets/sprites/disco.png",
+  "assets/sprites/djdisc.png",
+  "assets/sprites/drink.png",
+  // Riferimenti per la sequenza finale (anche se solo rettangoli, vanno cachati)
+  "assets/sprites/palo.png",
+  "assets/sprites/ragazza.png",
+  "assets/sprites/macchina.png",
 
   // musica
   "assets/music/music_normal.mp3",
