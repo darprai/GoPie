4215f21204dc53f7d619740aeaa541f0c95e1184
darprai <darioc.2000@gmail.com>
2025-12-06 17:40:50 +0100

    Refactor player class and helper functions

--- entities.js
+++ entities.js
@@ -1,33 +1,51 @@
-const playerSprite = new Image(); 
+// -----------------------------------------------------
+//  SPRITE PLAYER
+// -----------------------------------------------------
+const playerSprite = new Image();
 playerSprite.src = "assets/sprites/run.png";
 
-// --- helpers
-function rectsOverlap(a,b){
-  return !(a.x + a.w < b.x || a.x > b.x + b.w || a.y + a.h < b.y || a.y > b.y + b.h);
+// -----------------------------------------------------
+//  HELPERS
+// -----------------------------------------------------
+function rectsOverlap(a, b) {
+  return !(
+    a.x + a.w < b.x ||
+    a.x > b.x + b.w ||
+    a.y + a.h < b.y ||
+    a.y > b.y + b.h
+  );
 }
 
-// --- Player
+// -----------------------------------------------------
+//  PLAYER CLASS
+// -----------------------------------------------------
 class Player {
-  constructor(x,y){
-    this.x = x; 
+  constructor(x, y) {
+    this.x = x;
     this.y = y;
-    this.w = 40; 
+    this.w = 40;
     this.h = 56;
-    this.vx = 0; 
+    this.vx = 0;
     this.vy = 0;
     this.onGround = false;
-    this.color = '#ffd86b';
+    this.color = "#ffd86b"; // fallback color
     this.score = 0;
   }
 
-  update(dt, input, platforms){
+  update(dt, input, platforms) {
     const maxSpeed = 260;
     const jumpPower = 520;
 
-    let left  = input.keys['ArrowLeft'] || input.keys['a'] || input.touch.left;
-    let right = input.keys['ArrowRight'] || input.keys['d'] || input.touch.right;
-    let up    = input.keys['ArrowUp'] || input.keys['w'] || input.keys[' '] || input.touch.jump;
+    const left = input.keys["ArrowLeft"] || input.keys["a"] || input.touch.left;
+    const right =
+      input.keys["ArrowRight"] || input.keys["d"] || input.touch.right;
+    const up =
+      input.keys["ArrowUp"] ||
+      input.keys["w"] ||
+      input.keys[" "] ||
+      input.touch.jump;
 
+    // movement
     if (left && !right) this.vx = -maxSpeed;
     else if (right && !left) this.vx = maxSpeed;
     else this.vx = 0;
@@ -36,7 +54,7 @@ class Player {
     this.vy += 1400 * dt;
 
     // jump
-    if (up && this.onGround){
+    if (up && this.onGround) {
       this.vy = -jumpPower;
       this.onGround = false;
     }
@@ -47,14 +65,19 @@ class Player {
 
     // platform collisions
     this.onGround = false;
-    for (let p of platforms){
-      const plat = { x:p[0], y:p[1], w:p[2], h:p[3] };
-      if (this.x + this.w > plat.x && this.x < plat.x + plat.w){
-        if (this.y + this.h > plat.y && this.y + this.h < plat.y + plat.h + 20 && this.vy >= 0){
-          this.y = plat.y - this.h;
-          this.vy = 0;
-          this.onGround = true;
-        }
+    for (let p of platforms) {
+      const plat = { x: p[0], y: p[1], w: p[2], h: p[3] };
+
+      if (
+        this.x + this.w > plat.x &&
+        this.x < plat.x + plat.w &&
+        this.y + this.h > plat.y &&
+        this.y + this.h < plat.y + plat.h + 20 &&
+        this.vy >= 0
+      ) {
+        this.y = plat.y - this.h;
+        this.vy = 0;
+        this.onGround = true;
       }
     }
 
@@ -62,7 +85,7 @@ class Player {
   }
 
   draw(ctx, camX) {
-    if (playerSprite.complete) {
+    if (playerSprite.complete && playerSprite.width > 0) {
       ctx.drawImage(
         playerSprite,
         Math.round(this.x - camX),
@@ -71,6 +94,7 @@ class Player {
         this.h
       );
     } else {
+      // fallback rectangle
       ctx.fillStyle = this.color;
       ctx.fillRect(
         Math.round(this.x - camX),
@@ -82,5 +106,8 @@ class Player {
   }
 }
 
-// EXPORT PLAYER
+// -----------------------------------------------------
+//  EXPORT GLOBALE (IMPORTANTE!)
+// -----------------------------------------------------
 window.Player = Player;
+window.rectsOverlap = rectsOverlap;
