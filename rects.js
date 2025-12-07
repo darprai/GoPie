8a85539ddf986853105f4aff3e5f53a4c35f170a
darprai <darioc.2000@gmail.com>
2025-12-07 21:52:34 +0100

    Update rects.js

--- rects.js
+++ rects.js
@@ -1,9 +1,7 @@
-// Utilità per la verifica di collisione tra rettangoli (oggetti con x, y, w, h)
-function rectsOverlap(r1, r2) {
-  return r1.x < r2.x + r2.w &&
-         r1.x + r1.w > r2.x &&
-         r1.y < r2.y + r2.h &&
-         r1.y + r1.h > r2.y;
-}
-
-window.rectsOverlap = rectsOverlap; 
+// Funzione standard per controllare l'overlap di due rettangoli (objects con x, y, w, h)
+const rectsOverlap = (r1, r2) => {
+    return r1.x < r2.x + r2.w &&
+           r1.x + r1.w > r2.x &&
+           r1.y < r2.y + r2.h &&
+           r1.y + r1.h > r2.y;
+};
