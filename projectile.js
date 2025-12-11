9ae34dfa638648ddcda5ed9bb68b93194b5267b1
darprai <darioc.2000@gmail.com>
2025-12-11 19:01:28 +0100

    Update projectile.js

--- projectile.js
+++ projectile.js
@@ -1,29 +1,29 @@
 const Projectile = function(x, y, vx, vy) {
-    this.x = x;
-    this.y = y;
-    this.w = 20;
-    this.h = 20;
-    this.vx = vx;
-    this.vy = vy;
+    this.x = x;
+    this.y = y;
+    this.w = 20;
+    this.h = 20;
+    this.vx = vx;
+    this.vy = vy;
 
-    this.update = function(dt) {
-        // La velocità è scalata dal fattore delta tempo (dt)
-        this.x += this.vx * dt;
-        this.y += this.vy * dt;
-    };
+    this.update = function(dt) {
+        // La velocità è scalata dal fattore delta tempo (dt)
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+    };
 
-    this.draw = function(ctx, camX) {
-        const drawX = Math.round(this.x - camX);
-        const drawY = Math.round(this.y);
-        
-        if (window.drinkEnemySprite && window.drinkEnemySprite.complete) {
-            ctx.drawImage(window.drinkEnemySprite, drawX, drawY, this.w, this.h);
-        } else {
-            // Fallback: Disegna un cerchio rosso
-            ctx.fillStyle = 'red';
-            ctx.beginPath();
-            ctx.arc(drawX + this.w / 2, drawY + this.h / 2, this.w / 2, 0, Math.PI * 2);
-            ctx.fill();
-        }
-    };
+    this.draw = function(ctx, camX) {
+        const drawX = Math.round(this.x - camX);
+        const drawY = Math.round(this.y);
+        
+        if (window.drinkEnemySprite && window.drinkEnemySprite.complete) {
+            ctx.drawImage(window.drinkEnemySprite, drawX, drawY, this.w, this.h);
+        } else {
+            // Fallback: Disegna un cerchio rosso
+            ctx.fillStyle = 'red';
+            ctx.beginPath();
+            ctx.arc(drawX + this.w / 2, drawY + this.h / 2, this.w / 2, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    };
 };
